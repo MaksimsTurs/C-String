616db9576042b33df8672141cf9b98d543fe8d1d
Maksims Turs <maksims07turs@gmail.com>
2025-02-17 21:31:36 +0100

    Project structure changes, bug fix, some other performance and security update

--- SBString.c
+++ SBString.c
@@ -1,168 +1,205 @@
 #include "SBString.h"
 
-SB_Return_Code sb_string_create(SB_String* self, SB_CString string) {
-	SB_ULLong length = sb_string_length(string);
-	
-	self->length = length;
-	self->value = (SB_VString)malloc(length);
+SB_Return_Code sb_string_create(SB_String* self, const SB_String_Ptr string) {
+	if(self == NULL || string == NULL)
+		return SB_ERROR_INVALID_PTR;
+
+	SB_ULLong length = 0;
+
+	length = sb_string_length(string);
+	self->value = (SB_String_Ptr)malloc(length + 1);
 	if(self->value == NULL)
 		return SB_ERROR_MEMALLOC;
 	
 	memcpy(self->value, string, length);
 	self->value[length] = '\0';
+	self->length = length;
 
 	return SB_SUCCESS;
 }
 
-SB_Return_Code sb_string_delete(SB_String self) {
-	if(self.value == NULL)
-		return SB_ERROR_NULL_PTR;
+SB_Return_Code sb_string_free(SB_String* self) {
+	if(self == NULL || self->value == NULL)
+		return SB_ERROR_INVALID_PTR;
 
-	free(self.value);
+	free(self->value);
+	self->value = NULL;
+	self->length = 0;
 
 	return SB_SUCCESS;
 }
 
-SB_Return_Code sb_string_append(SB_String* self, SB_CString append_string, SB_UShort count) {
-	SB_ULLong append_length = sb_string_length(append_string);
-	SB_ULLong string_new_length = self->length + append_length * count;
-	SB_UShort append_count = !count ? 1 : count;
+SB_Return_Code sb_string_split(SB_Split* split, const SB_String_Ptr to_split, const SB_Char spliter) {
+	if(split == NULL || to_split == NULL)
+		return SB_ERROR_INVALID_PTR;
 
-	self->value = (SB_VString)realloc(self->value, string_new_length * append_count);
-	if(self->value == NULL)
-		return SB_ERROR_MEMALLOC;
+	SB_Char ch = ' ';
+	SB_String_Ptr item = NULL;
+	SB_String_Ptr* items = NULL;
+	SB_String_Ptr c_to_split = NULL;
+	SB_UShort splited_length = 0;
 	
-	self->value += self->length;
-	if(append_count == 1) {
-		memcpy(self->value, append_string, append_length);
-	} else {
-		SB_UShort index = 0;
+	split->items = (SB_String_Ptr*)malloc(1);
+	c_to_split = to_split;
 
-		while(index < append_count) {
-			memcpy(self->value + (append_length * index), append_string, append_length);
-			index++;
+	while(SB_TRUE) {
+		ch = *c_to_split;
+		if((ch == spliter || ch == SB_NULL_TERMINATOR) && splited_length > 0) {
+			// Skip spliter.
+			c_to_split++;
+
+			// Realloc memory for one new element.
+			items = (SB_String_Ptr*)realloc(split->items, (sizeof(SB_String_Ptr) * split->size) + 1);
+			if(items == NULL)
+				return SB_ERROR_MEMALLOC;
+			split->items = items;
+
+			// Alloc memory for new array item.
+			item = (SB_String_Ptr)malloc(splited_length);
+			if(item == NULL)
+				return SB_ERROR_MEMALLOC;
+
+			memcpy(item, (c_to_split - splited_length) - 1, splited_length);
+			item[splited_length] = '\0';
+			
+			splited_length = 0;
+
+			split->items[split->size++] = item;
+		}
+
+		if(ch == SB_NULL_TERMINATOR)
+			break;
+
+		splited_length++;
+		c_to_split++;
+	}
+
+	return SB_SUCCESS;
+}
+
+SB_Return_Code sb_string_split_free(SB_Split* self) {
+	if(self == NULL || self->items == NULL)
+		return SB_ERROR_INVALID_PTR;
+
+	for(SB_UShort index = 0; index < self->size; index++) {
+		if(self->items[index] != NULL) {
+			free(self->items[index]);
+			self->items[index] = NULL;
 		}
 	}
-	self->value -= self->length;
+
+	free(self->items);
+	self->items = NULL;
+
+	return SB_SUCCESS;
+}
+
+SB_Return_Code sb_string_append(SB_String* self, const SB_String_Ptr append_string, const SB_UChar count) {
+	if(self == NULL || self->value == NULL || append_string == NULL)
+		return SB_ERROR_INVALID_PTR;
+	else if(count == 0)
+		return SB_ERROR_INVALID_COUNT;
+
+	SB_ULLong append_length = 0;
+	SB_ULLong string_new_length = 0;
+	SB_UShort index = 0;
+	SB_String_Ptr new_value = NULL;
+	SB_String_Ptr dest = NULL;
+
+	append_length = sb_string_length(append_string);
+	string_new_length = self->length + (append_length * count);
+
+	new_value = (SB_String_Ptr)realloc(self->value, string_new_length + 1);
+	if(new_value == NULL)
+		return SB_ERROR_MEMALLOC;
+	self->value = new_value;
 	
+	// Set position to end of the old string.
+	dest = new_value + self->length;
+	for(index = 0; index < count; index++)
+		memcpy(dest + (append_length * index), append_string, append_length);
+
 	self->length = string_new_length;
 	self->value[self->length] = '\0';
 
 	return SB_SUCCESS;
 }
 
-SB_Return_Code sb_string_repeat(SB_String* self, SB_CString repeater, SB_UShort count) {
-	if(count == 0) 
-		return SB_ERROR_INVALID_COUNT;
+SB_Bool sb_string_compare(const SB_String_Ptr str_one, const SB_String_Ptr str_two) {
+	if(str_one == NULL || str_two == NULL)
+		return SB_FALSE;
 
-	SB_ULLong repeater_length = sb_string_length(repeater);
-	SB_ULLong new_string_length = self->length + (count * repeater_length);
-	SB_UShort index = 0;
+	const SB_UChar *str_ptr_one = (const SB_UChar*)str_one;
+	const SB_UChar *str_ptr_two = (const SB_UChar*)str_two;
 
-	self->value = (SB_VString)realloc(self->value, new_string_length);
-	if(self->value == NULL)
-		return SB_ERROR_MEMALLOC;
+	SB_UChar ch_one = 0;
+	SB_UChar ch_two = 0;
+	SB_Bool is_equal = SB_TRUE;
 
-	self->value += self->length;
-	if(count == 1) {
-		memcpy(self->value + (repeater_length * index), repeater, repeater_length);
-	} else {
-		while(index < count) {
-			memcpy(self->value + (repeater_length * index), repeater, repeater_length);
-			index++;
+	do {
+		ch_one = (SB_UChar)*str_ptr_one++;
+		ch_two = (SB_UChar)*str_ptr_two++;
+
+		if(ch_one != ch_two) {
+			is_equal = SB_FALSE;
+			break;
 		}
-	}
-	self->value -= self->length;
+	} while(*str_ptr_one != SB_NULL_TERMINATOR || *str_ptr_two != SB_NULL_TERMINATOR);
 
-	self->length = new_string_length;
-	self->value[new_string_length] = '\0';
-
-	return SB_SUCCESS;
+	return is_equal;
 }
 
-SB_ULLong sb_string_index_of(SB_String* self, SB_CChar ch, SB_ULLong start_position) {
-	for(SB_ULLong position = start_position; position < self->length; position++) {
-		if(self->value[position] == ch)
-			return position;
-	}
+SB_ULLong sb_string_length(const SB_String_Ptr self) {
+	if(self == NULL)
+		return 0;
 
-	return 0;
+	SB_String_Ptr string_cp = self;
+
+	while(*string_cp != SB_NULL_TERMINATOR)
+		string_cp++;
+	
+	return(string_cp - self);
 }
 
-SB_Return_Code sb_string_slice(SB_String* self, SB_ULLong start, SB_ULLong end) {
+SB_LLong sb_string_index_of(SB_String* self, const SB_Char ch, const SB_ULLong start_position) {
+	if(self == NULL || self->value == NULL)
+		return SB_ERROR_INVALID_PTR;
+
+	SB_String_Ptr self_cp = self->value + start_position;
+	SB_LLong ch_position = -1;
+
+	while(SB_TRUE) {
+		if(*self_cp == SB_NULL_TERMINATOR && ch != SB_NULL_TERMINATOR) {
+			break;
+		}	else if(*self_cp == ch) {
+			ch_position = self_cp - self->value;
+			break;
+		}
+		self_cp++;
+	}
+
+	return ch_position;
+}
+
+SB_Return_Code sb_string_slice(SB_String* self, const SB_ULLong start, const SB_ULLong end) {
 	if(start > self->length || end > self->length || start > end)
 		return SB_ERROR_INVALID_POSITION;
+	else if(self == NULL || self->value == NULL)
+		return SB_ERROR_INVALID_PTR;
 
 	SB_ULLong new_string_length = (end - start);
+	SB_String_Ptr new_str = NULL;
 
 	self->length = new_string_length;
-	SB_VString new_str = (SB_VString)malloc(new_string_length + 1);
-	if(self->value == NULL)
+	new_str = (SB_String_Ptr)malloc(new_string_length + 1);
+	if(new_str == NULL)
 		return SB_ERROR_MEMALLOC;
 
-	memcpy(new_str, self->value + start, new_string_length + 1);
+	memcpy(new_str, self->value + start, new_string_length);
 	free(self->value);
+	
 	self->value = new_str;
 	self->value[self->length] = '\0';
 
 	return SB_SUCCESS;
-}
-
-SB_ULLong sb_string_length(SB_CString self) {
-	SB_ULLong length = 0;
-	SB_CString string_cp = self;
-
-	while(*string_cp++ != SB_NULL_TERMINATOR)
-		length++;
-
-	return length;
-}
-
-SB_Return_Code sb_string_split_delete(SB_Split self) {
-	for(SB_UShort index = 0; index < self.length; index++) {
-		if(self.item[index] != NULL)
-			free((void*)self.item[index]);
-	}
-
-	free(self.item);
-
-	return SB_SUCCESS;
-}
-
-SB_Return_Code sb_string_split(SB_Split* split, SB_CString to_split, SB_CChar spliter) {
-	SB_Char ch = ' ';
-	SB_VString item = NULL;
-	SB_UShort ch_count = 0;
-	
-	split->item = (SB_CString*)malloc(1);
-
-	while(ch != '\0') {
-		ch = *to_split;
-		if(ch == spliter || ch == '\0') {
-			// Skip spliter.
-			to_split++;
-
-			split->length++;
-			split->item = (SB_CString*)realloc(split->item, (sizeof(SB_CString) * split->length) + 1);
-			if(split->item == NULL)
-				return SB_ERROR_MEMALLOC;
-			item = (SB_VString)malloc(ch_count);
-			if(split->item == NULL)
-				return SB_ERROR_MEMALLOC;
-
-			memcpy(item, (to_split - ch_count) - 1, ch_count);
-			item[ch_count] = '\0';
-			split->item[split->length - 1] = item;
-			ch_count = 0;
-		}
-
-		ch_count++;
-		to_split++;
-	}
-
-	return SB_SUCCESS;
-}
-
-SB_Boolean sb_string_compare(SB_CString str_one, SB_CString str_two) {
-	return strcmp(str_one, str_two) == 0;
 }
--- SBString.h
+++ SBString.h
@@ -1,56 +1,50 @@
+#pragma once
+/*=======================================================================*/
 #include <stdlib.h>
 #include <string.h>
-
-// Types constants.
-#define SB_TRUE  (unsigned char)1
-#define SB_FALSE (unsigned char)0
-
-// Error and success constants.
-#define SB_ERROR_MEMALLOC         (unsigned char)0
-#define SB_ERROR_NULL_PTR         (unsigned char)2
-#define SB_ERROR_INVALID_POSITION (unsigned char)3
-#define SB_ERROR_INVALID_COUNT    (unsigned char)4
-#define SB_SUCCESS                (unsigned char)1
-
-// Character constants.
-#define SB_NULL_TERMINATOR '\0'
-
-/*===========================================*/
+/*=======================================================================*/
 typedef unsigned char      SB_Return_Code;
-/*===========================================*/
-typedef unsigned char      SB_Boolean;
-/*===========================================*/
-typedef char*              SB_VString;
-typedef const char*        SB_CString;
-/*===========================================*/
-typedef const char         SB_CChar;
+typedef unsigned char      SB_Bool;
+typedef unsigned char      SB_UChar;
+typedef char*              SB_String_Ptr;
 typedef char               SB_Char;
-/*===========================================*/
+
 typedef unsigned long long SB_ULLong;
 typedef long long          SB_LLong;
-/*===========================================*/
-typedef unsigned short     SB_UShort;
-/*===========================================*/
 
-typedef struct SB_String {
-	SB_VString value;
+typedef unsigned short     SB_UShort;
+/*=======================================================================*/
+// Types constants.
+#define SB_TRUE                   (SB_UChar)1
+#define SB_FALSE                  (SB_UChar)0
+// Error/success constants.
+#define SB_ERROR_MEMALLOC         (SB_UChar)0
+#define SB_ERROR_INVALID_PTR      (SB_UChar)2
+#define SB_ERROR_INVALID_POSITION (SB_UChar)3
+#define SB_ERROR_INVALID_COUNT    (SB_UChar)4
+#define SB_SUCCESS                (SB_UChar)1
+// Character constants.
+#define SB_NULL_TERMINATOR        (SB_UChar)'\0'
+/*=======================================================================*/
+typedef struct {
+	SB_String_Ptr value;
 	SB_ULLong length;
 } SB_String;
 
-typedef struct SB_Split {
-	SB_CString* item;
-	SB_UShort length;
+typedef struct {
+	SB_String_Ptr* items;
+	SB_UShort size;
 } SB_Split;
+/*=======================================================================*/
+SB_Return_Code sb_string_create(SB_String* self, const SB_String_Ptr string);
+SB_Return_Code sb_string_free(SB_String* self);
+SB_Return_Code sb_string_split(SB_Split* split, const SB_String_Ptr to_split, const SB_Char spliter);
+SB_Return_Code sb_string_split_free(SB_Split* self);
+SB_Return_Code sb_string_append(SB_String* self, const SB_String_Ptr append_string, const SB_UChar count);
+SB_Return_Code sb_string_slice(SB_String* self, const SB_ULLong start, const SB_ULLong end);
 
-SB_Return_Code sb_string_create(SB_String* self, SB_CString string);
-SB_Return_Code sb_string_delete(SB_String self);
-SB_Return_Code sb_string_split_delete(SB_Split self);
-SB_Return_Code sb_string_split(SB_Split* split, SB_CString to_split, SB_CChar spliter);
-SB_Return_Code sb_string_append(SB_String* self, SB_CString append_string, SB_UShort count);
-SB_Return_Code sb_string_slice(SB_String* self, SB_ULLong start, SB_ULLong end);
-SB_Return_Code sb_string_repeat(SB_String* self, SB_CString repeater, SB_UShort count);
+SB_ULLong sb_string_length(const SB_String_Ptr self);
+SB_LLong sb_string_index_of(SB_String* self, const SB_Char ch, const SB_ULLong start_position);
 
-SB_ULLong sb_string_length(SB_CString self);
-SB_ULLong sb_string_index_of(SB_String* self, SB_CChar ch, SB_ULLong start_position);
-
-SB_Boolean sb_string_compare(SB_CString str_one, SB_CString str_two);
+SB_Bool sb_string_compare(const SB_String_Ptr str_one, const SB_String_Ptr str_two);
+/*=======================================================================*/
